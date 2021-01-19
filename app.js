8d02b446242405ac39febd8925417ca353123317
RijlanM10 <rijlan01@gmail.com>
2021-01-20 03:00:34 +0700

    Serving HTML Pages

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 var server = http.createServer(function (req, res) {
     console.log('request was made: ' + req.url);
     res.writeHead(200, {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/html'
     });
-    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
 
     myReadStream.pipe(res);
 });
