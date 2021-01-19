4a42a42704de2102418a7fee1498962b81614a48
RijlanM10 <rijlan01@gmail.com>
2021-01-20 01:25:46 +0700

    Creating & Deleting Directories

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var fs = require('fs');
 
-fs.readFile('readMe.txt', 'utf8', (err, data) => {
-    fs.writeFile('writeMe.txt', data, err => {
+// fs.unlink('writeMe.txt', err => {
 
+// });
+
+// fs.mkdir('stuff', () => {
+//     fs.readFile('readMe.txt', 'utf8', (err, data) => {
+//         fs.writeFile('./stuff/writeMe.txt', data, err => {
+
+//         });
+//     });
+// });
+
+fs.unlink('./stuff/writeMe.txt', () => {
+    fs.rmdir('stuff', err => {
+        
     });
 });
