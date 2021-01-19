ae42418b406c5603c323d9c68e0b3f6a2be50c3d
RijlanM10 <rijlan01@gmail.com>
2021-01-20 02:46:17 +0700

    Writeable Streams

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var http = require('http');
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 myReadStream.on('data', function(chunk) {
-    console.log('new chunk received: ');
-    console.log(chunk);
+    console.log('new chunk received');
+    myWriteStream.write(chunk);
 });
