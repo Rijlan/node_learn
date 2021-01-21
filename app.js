11c4269145ecddd60051a48df4c53b5bb5f835db
RijlanM10 <rijlan01@gmail.com>
2021-01-21 09:09:37 +0700

    Template Engine

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('this is the homepage');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', (req, res) => {
-    res.send('this is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:name', (req, res) => {
-    res.send('You requested to see a profile with the name of ' + req.params.name);
+    var data = {age: 29, job: 'ninja'};
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
