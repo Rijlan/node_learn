242302c476ff04547632636a9e2777da9024ae30
RijlanM10 <rijlan01@gmail.com>
2021-01-21 08:49:45 +0700

    Introduction Express

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 
-var server = http.createServer(function (req, res) {
-    console.log('request was made: ' + req.url);
-    if (req.url === '/home' || req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } else if (req.url === '/contact-us') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/contact.html').pipe(res);
-    } else if (req.url === '/api/ninjas') {
-        var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
+var app = express();
 
-        res.writeHead(200, {'Content-Type': 'application.json'});
-        res.end(JSON.stringify(ninjas));
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/404.html').pipe(res);
-    }
+app.get('/', (req, res) => {
+    res.send('this is the homepage');
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
+app.get('/contact', (req, res) => {
+    res.send('this is the contact page');
+});
+
+app.listen(3000);
