e353906bde243a2350c2ccc3dd6dbae31e53184f
RijlanM10 <rijlan01@gmail.com>
2021-01-21 08:03:34 +0700

    Serving JSON

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var fs = require('fs');
 var server = http.createServer(function (req, res) {
     console.log('request was made: ' + req.url);
     res.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'application/json'
     });
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
 
-    myReadStream.pipe(res);
+    var myObj = {
+        name: 'Ryu',
+        job: 'Ninja',
+        age: 29
+    };
+
+    res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');
