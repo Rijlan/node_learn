396d8b6b72418dea97103348e6ecf5c60e4a0045
RijlanM10 <rijlan01@gmail.com>
2021-01-21 08:55:46 +0700

    Express Route Params

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.get('/contact', (req, res) => {
     res.send('this is the contact page');
 });
 
+app.get('/profile/:name', (req, res) => {
+    res.send('You requested to see a profile with the name of ' + req.params.name);
+});
+
 app.listen(3000);
