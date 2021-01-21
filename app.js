b0dd73a3fa28cd1df0d5e0afb99633d171523399
RijlanM10 <rijlan01@gmail.com>
2021-01-21 09:40:23 +0700

    Middleware & Static Files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('stuff'));
 
 app.get('/', (req, res) => {
     res.render('index');
