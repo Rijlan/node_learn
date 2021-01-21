bbd0282f63a2cf0aecb23b9edf4f52a50f3695b8
RijlanM10 <rijlan01@gmail.com>
2021-01-21 08:16:35 +0700

    Basic Routing

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
+    if (req.url === '/home' || req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/contact') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if (req.url === '/api/ninjas') {
+        var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
 
-    var myObj = {
-        name: 'Ryu',
-        job: 'Ninja',
-        age: 29
-    };
-
-    res.end(JSON.stringify(myObj));
+        res.writeHead(200, {'Content-Type': 'application.json'});
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
 });
 
 server.listen(3000, '127.0.0.1');
